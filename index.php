521792a09e11035d35b5059bad7b04fb3be71942
Wiwin Hartanto <me@wiwinhartanto.com>
2018-04-15 07:03:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<script>
+    window.intergramId = 95498949;
+    window.intergramServer = "https://whdotcombot.herokuapp.com";
+
+</script>
+<script src="https://chat.wiwinhartanto.com/js/widget.js"></script>
